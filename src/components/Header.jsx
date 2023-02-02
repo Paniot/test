8806945c7a59149432f374a4e6401ef6b728ca6f
Serhii <paniot96@yahoo.com>
2023-02-02 14:20:04 -0500

    02.02.2023

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from './../img/logo.png'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={Logo} alt="hmm"></img>
+    </header>)
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import Picture from './../img/profile.jpg'
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="content-img">
+        <img src={Picture} alt="" className='image'></img>
+      </div>
+      <div>ava + des</div>
+      <div>my posts
+        <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  )
+};
+
+export default Profile;
