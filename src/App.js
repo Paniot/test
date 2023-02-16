2850f3e47373e2571d733a45bc359a53f9fd3aa5
Serhii <paniot96@yahoo.com>
2023-02-15 19:17:34 -0600

    15-Feb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
@@ -7,10 +8,11 @@ import News from './components/News/News'
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { addPost } from './redux/state';
 
 
 
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -18,8 +20,12 @@ const App = () => {
         <NavBar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path='/dialogs' element={<Dialogs />} />
-            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs
+              dialogsData={props.state.dialogsPage.dialogsData}
+              messagesData={props.state.dialogsPage.messagesData} />} />
+            <Route path='/profile' element={<Profile
+              postData={props.state.profilePage.postData}
+              addPost={props.addPost} />} />
             <Route path='/news' element={<News />} />
             <Route path='/music' element={<Music />} />
             <Route path='/settings' element={<Settings />} />
